1b49061ff04f80d625e077dac11418ebf1e26d09
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-10-13 14:26:11 +0200

    adding frontend url in .env

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ app.use((err, req, res, next) => {
 });
 // enabling CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', process.env.FRONTED_URL || 'http://localhost:3000') // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     if (req.method === 'OPTIONS') {
