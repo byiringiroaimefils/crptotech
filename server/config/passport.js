99533b755029b8ed0c5b5b3f9abcf8a42cae8041
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-10-13 15:48:39 +0200

    update Google OAuth callback URL for production environment

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/api/auth/google/callback",
+       callbackURL:
+      process.env.NODE_ENV === "production"
+        ? "https://crptotech.onrender.com/api/auth/google/callback"
+        : "http://localhost:3001/api/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
